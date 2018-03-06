cd5c70c0623f843c165d19a0f8429fd0b745f7ef
acailly <ylliac14@gmail.com>
2018-03-06 17:01:04 +0100

    :tada: First version

---
+++ index.js
@@ -0,0 +1,28 @@
+const trending = require("trending-github");
+const chalk = require("chalk");
+
+module.exports = function(vorpal) {
+  vorpal
+    .command("github-trends")
+    .alias("gt")
+    .description("Show today's github trends")
+    .action(function(args, callback) {
+      trending("daily", "javascript")
+        .then(repos => {
+          const result = repos
+            .slice(0, 10)
+            .map(item => {
+              const name = chalk.green(`${item.author} / ${item.name}`);
+              const description = item.description;
+              const url = chalk.blue(item.href);
+              const stars = chalk.yellow(item.stars + " stars");
+
+              return `${name}\t ${stars}\n ${description}\n (${url})\n`;
+            })
+            .join("\n");
+
+          callback(result);
+        })
+        .catch(e => callback(e));
+    });
+};
